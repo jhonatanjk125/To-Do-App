dccc2fb8d420e4649a99f98ba1aba4cb4a189054
jhonatanjk125 <jhonatanjk125@gmail.com>
2023-06-02 20:10:15 -0500

    Implement the Edit feature

--- gui.py
+++ gui.py
@@ -3,8 +3,11 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a to-do", key="user_input")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos-list', enable_events=True, size=[45,10])
+edit_button = sg.Button("Edit")
+
 window = sg.Window('To Do - List',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Gill Sans', 15))
 while True:
     event, values = window.read()
@@ -15,7 +18,18 @@ while True:
             todos = functions.get_todos()
             todos.append(values['user_input'] + "\n")
             functions.write_todos(todos)
-        case sg.WIN_CLOSED:
+            window['todos-list'].update(values=todos)
+        case "Edit":
+            todo_to_edit=values['todos-list'][0]
+            new_todo = values['user_input'] + "\n"
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos-list'].update(values=todos)
+        case 'todos-list':
+            window['user_input'].update(value=values['todos-list'][0])
+        case   sg.WIN_CLOSED:
             break
 
 window.close()

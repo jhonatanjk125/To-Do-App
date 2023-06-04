9e76c2ad5481d853c2a41ab69504f1f03979c581
jhonatanjk125 <jhonatanjk125@gmail.com>
2023-06-04 15:21:57 -0500

    Implement the Complete and Exit features

--- gui.py
+++ gui.py
@@ -5,9 +5,14 @@ input_box = sg.InputText(tooltip="Enter a to-do", key="user_input")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos-list', enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
 
 window = sg.Window('To Do - List',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label], [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Gill Sans', 15))
 while True:
     event, values = window.read()
@@ -27,8 +32,17 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos-list'].update(values=todos)
+        case "Complete":
+            todo_to_complete=values['todos-list'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos-list'].update(values=todos)
+            window['user_input'].update(value="")
         case 'todos-list':
             window['user_input'].update(value=values['todos-list'][0])
+        case "Exit":
+            break
         case   sg.WIN_CLOSED:
             break
 
